621d2c2166ec4cfda0276fbe0e5189a3be5dcda9
wentran <wen.tran1@gmail.com>
2018-02-15 22:15:04 -0500

    added server file

--- server.js
+++ server.js
@@ -12,18 +12,32 @@ const port = 3000;
 
 app.use(handleRender);
 
+//invoked evertime client requests to app
 function handleRender (req, res) {
 	const store = createStore(counterApp);
 
-//render the component to a string for SEO crawler and performance boost
+//render the component to a string for SEO crawler and performance boost - App as root component to be wrapped inside a provider to make the store available to all components in the component tree
+
+//render initial HTML of component before we send it to clinet sire
 const html = renderToString(
 	<Provider store={store}>
 	  <App />
 	</Provider>
 	);
 
+//grab initial state from Redux store
 const preloadedState = store.getState();
 
+//send the rendered page back to the client.
 res.send(renderFullPage(html, preloadedState))
-
 };
+
+
+function renderFullPage (html, preloadedState) {
+	return '
+    <!doctype html>
+    <html>
+      <head>
+
+	'
+}
